8cac43c86bd5467b841f233a908954afdf9ef1a0
witch641 <130225027+witch641@users.noreply.github.com>
2024-11-06 10:15:21 +0800

    Add files via upload

--- components/homepage/Intro.tsx
+++ components/homepage/Intro.tsx
@@ -126,7 +126,7 @@ export default function Intro() {
         }}
       >
         <a
-          href="/Silvia-Frontend.pdf"
+          href="/Silvia-Siyi-Fresh.pdf"
           download={true}
           className="group text-sm  px-4 py-2  bg-white sm:text-lg sm:px-7 sm:py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
         >
