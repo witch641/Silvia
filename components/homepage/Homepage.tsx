8ef5853f6f515f95c8097a495bb2aafc0f06edba
Siyi Liu <130225027+witch641@users.noreply.github.com>
2024-11-12 20:55:39 +0800

    Add files via upload

---
+++ components/homepage/Homepage.tsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+import Hobbies from "./Hobbies";
+// ...existing code...
+
+const Homepage = () => {
+  // ...existing code...
+  return (
+    <div>
+      // ...existing code...
+      <Hobbies />
+      // ...existing code...
+    </div>
+  );
+};
+
+export default Homepage;
